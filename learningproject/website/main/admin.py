fea3d71ba309189e81c1e322f507e747a6c00fbb
Matthew Polak <polak.matthew2003@gmail.com>
2023-11-11 11:08:08 -0600

    updated quiz and subject models

--- learningproject/website/main/admin.py
+++ learningproject/website/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import AttemptedAnswer, Post, Reply, User, File, Subject, Question, Attempt, Answer
+from .models import AttemptedAnswer, Post, Reply, User, File, Quiz, Question, Attempt, Answer
 
 #these models are displayed and can be manipulated
 #in the admin panel
@@ -7,7 +7,7 @@ admin.site.register(Post)
 admin.site.register(Reply)
 admin.site.register(User)
 admin.site.register(File)
-admin.site.register(Subject)
+admin.site.register(Quiz)
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Attempt)
